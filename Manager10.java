3a6daf94534d3accf09372a4bd4d39d4ce8338d2
BASKAR.K <110029191+Baskarkumar2313@users.noreply.github.com>
2022-09-20 11:10:32 +0530

    Add files via upload

---
+++ Manager10.java
@@ -0,0 +1,18 @@
+public class Manager10
+{
+public static void main(String[] args)
+{
+int arr[]={4,2,1,2,2,0,6,2,2,0,0,5};
+System.out.println(arr[0]);
+System.out.println(arr[1]);
+System.out.println(arr[2]);
+System.out.println(arr[3]);
+System.out.println(arr[4]);
+System.out.println(arr[5]);
+System.out.println(arr[6]);
+System.out.println(arr[7]);
+System.out.println(arr[8]);
+System.out.println(arr[9]);
+System.out.println("Done");
+}
+}
